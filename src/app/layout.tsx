7f7a7ea6f7941c899b086b47a1e7e50332390031
ANVI JADAV <at41rv@gmail.com>
2026-01-03 23:56:29 +0530

    Revert theme to system default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,8 +52,8 @@ export default function RootLayout({
       <body className={`${hkGrotesk.className} ${instrumentSerif.variable}`} suppressHydrationWarning={true}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           <div className="relative z-10">
